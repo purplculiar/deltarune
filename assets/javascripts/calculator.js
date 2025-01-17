e8a445e511f0a2073c1ab8a0dcd66a6e1d5b45f2
purplculiar <conihamachan@gmail.com>
2025-01-17 16:49:07 -0500

    WHATS YOUR FAVORITE BLOOD TYPE

--- assets/javascripts/calculator.js
+++ assets/javascripts/calculator.js
@@ -63,7 +63,6 @@ function calculate() {
 	else {
 		var chance = 100 / (365 - (Math.floor((new Date() - new Date('2025-01-01')) / (1000 * 60 * 60 * 24))));
 		var millichance = 100 / (31556952000 - (Math.floor((new Date() - new Date('2025-01-01')))));
-		var millichance = millichance.toFixed();
 		$('#chance').html(`${Math.round((chance + Number.EPSILON) * 100) / 100}% or`);
 		$('#accurate-chance').html(`${chance}% for the nerds`);
 		$('#milli-chance').html(`And for fun theres a ${millichance}% it releases at this very millisecond.`);
